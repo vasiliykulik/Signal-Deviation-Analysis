785d174df128f1a31ca4d2015caa2c8fe98978ba
Vasiliy Kylik <detroit.vv@gmail.com>
2018-03-01 14:51:33 +0200

    linkToInfoPage - parsed only once, after value just assigned

--- ua/sda/java/Main.java
+++ ua/sda/java/Main.java
@@ -16,7 +16,6 @@ public class Main {
         final String password = args[2];
 
         // Reading modems (street, houseNumber, linkToMAC) from "TraficLight"
-
         List<Modem> modems = new ArrayList<Modem>();
         OpticalNodeSingleInterfaceReader opticalNodeSingleInterfaceReader = new OpticalNodeSingleInterfaceReader();
         try {
