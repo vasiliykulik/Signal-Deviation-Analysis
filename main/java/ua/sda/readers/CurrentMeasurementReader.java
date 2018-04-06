78ff2d80c9ce7a195c1b5ed546fa07418707d5e1
Raketa <detroi.vv@gmail.com>
2018-04-06 22:34:49 +0300

    CurrentStateReader

--- main/java/ua/sda/readers/CurrentMeasurementReader.java
+++ main/java/ua/sda/readers/CurrentMeasurementReader.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static ua.sda.cleaners.CleanerForCurrentState.cleanerForCurrentState;
 
@@ -35,12 +37,9 @@ public class CurrentMeasurementReader {
 		URL url = new URL(linkToURL);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-		// set up url connection to get retrieve information back
 		con.setRequestMethod("GET");
 		con.setDoInput(true);
 
-		// stuff the Authorization request header
-		// the password to the encoder, encoder to the connection, StreamReader take InputStream from connection and to the BufferedReader
 		byte[] encodedPassword = (userName + ":" + password).getBytes();
 		BASE64Encoder encoder = new BASE64Encoder();
 		con.setRequestProperty("Authorization",
@@ -50,14 +49,46 @@ public class CurrentMeasurementReader {
 		try (BufferedReader in = new BufferedReader(
 				new InputStreamReader(con.getInputStream(), "koi8_r"))) {
 
+			Float usTXPower = 0f;
+			Float usRXPower = 0f;
+			Float dsRxPower = 0f;
+			Float usSNR = 0f;
+			Float dsSNR = 0f;
+			Float microReflex = 0f;
 			String inputLine;
 			CleanerForCurrentState cleanerForCurrentState = new CleanerForCurrentState();
-			while ((inputLine = in.readLine()) != null && (in.lines().count() == 238 - 1 || in.lines().count() == 224 - 1)) {
+			// Получаем страницу, если модем онлайн (in.lines().count() == 238 - 1 || in.lines().count() == 224 - 1))
+			// снимаем измерения
+			// проверяем можем ли мы снять измерения (если нет бросаем exception)
+			if (in.lines().count() != 238 - 1 || in.lines().count() != 224 - 1){
+				throw new Exception("modem is not online");
+			}
+			while ((inputLine = in.readLine()) != null
+					// && (in.lines().count() == 238 - 1 || in.lines().count() == 224 - 1)
+					) {
 				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Upstream TX Power </td>")) {
-					Float usTXPower = cleanerForCurrentState(in.readLine());
+					currentState.setUsTXPower (cleanerForCurrentState(in.readLine()));
+				}
+				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Upstream RX Power, Iface PL </td>")) {
+					currentState.setUsRXPower(cleanerForCurrentState(in.readLine()));
+				}
+				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Upstream SNR </td>")) {
+					currentState.setUsSNR(cleanerForCurrentState(in.readLine()));;
+				}
+				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Downstream RX Power </td>")) {
+					currentState.setUsRXPower (cleanerForCurrentState(in.readLine()));
+				}
+				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Downstream SNR </td>")) {
+					currentState.setDsSNR(cleanerForCurrentState(in.readLine()));
+				}
+				if (inputLine.matches("<td bgcolor=\"#......\" colspan=\"2\">  Micro Reflx </td>")) {
+					currentState.setMicroReflex(cleanerForCurrentState(in.readLine()));
 				}
 			}
+			currentState.setDateTime(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(String.valueOf(new Date())));
+
 		}
+
 		return currentState;
 	}
 }
