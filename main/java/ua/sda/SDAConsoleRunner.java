e480b42d31a96c454354fe75851d5eec672fe956
Vasiliy Kylik <detroit.vv@gmail.com>
2018-04-14 21:16:05 +0300

    SDAConsoleRunner

---
+++ main/java/ua/sda/SDAConsoleRunner.java
@@ -0,0 +1,11 @@
+package ua.sda;
+
+import ua.sda.helper.ConsoleHelper;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 14.04.2018.
+ */
+public class SDAConsoleRunner {
+    ConsoleHelper consoleHelper = new ConsoleHelper();
+        consoleHelper.consoleHelp();
+}
---
+++ main/java/ua/sda/helper/ConsoleHelper.java
@@ -0,0 +1,7 @@
+package ua.sda.helper;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 14.04.2018.
+ */
+public class ConsoleHelper {
+}
