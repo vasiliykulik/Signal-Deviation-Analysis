b6a54271df385389feb99b35972eaf3b634fa682
Vasiliy Kylik <detroit.vv@gmail.com>
2018-04-23 17:19:43 +0300

    RetrieveDataView, AnalyzeDataView, DataView

--- main/java/ua/sda/view/RetrieveDataView.java
+++ main/java/ua/sda/view/RetrieveDataView.java
@@ -1,5 +1,14 @@
 package ua.sda.view;
 
+import ua.sda.SDAConsoleRunner;
+import ua.sda.controller.RetrieveDataController;
+import ua.sda.view.helper.ConsoleHelper;
+
+import java.io.IOException;
+
+import static ua.sda.view.helper.ConsoleHelper.readInt;
+import static ua.sda.view.helper.ConsoleHelper.writeMessage;
+
 /**
  * Created by Vasiliy Kylik (Lightning) on 19.04.2018.
  */
@@ -7,7 +16,39 @@ package ua.sda.view;
         String linkToInterface = readString();*/
 
 public class RetrieveDataView {
-    public void execute() {
+    public void execute() throws IOException {
+        RetrieveDataController retrieveDataController = new RetrieveDataController();
 
+        writeMessage("" +
+                "0 - \n" +
+                "1 - \n" +
+                "2 - \n" +
+                "3 - \n" +
+                "4 - \n" +
+                "5 - \n" +
+                "6 - Exit to the main menu\n");
+
+        int choice = readInt();
+
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                writeMessage("\n Exit to the main menu...\n");
+                break;
+            default:
+                break;
+        }
+        execute();
     }
 }
