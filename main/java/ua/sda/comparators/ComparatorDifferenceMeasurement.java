e917e638c5231495d7a54fc6b3479be39c850cb9
Vasiliy Kylik <detroit.vv@gmail.com>
2018-06-13 10:55:37 +0300

    ComparatorDifferenceMeasurement

---
+++ main/java/ua/sda/comparators/ComparatorDifferenceMeasurement.java
@@ -0,0 +1,15 @@
+package ua.sda.comparators;
+
+import ua.sda.entity.opticalnodeinterface.Measurement;
+
+import java.util.Comparator;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 13.06.2018.
+ */
+public class ComparatorDifferenceMeasurement implements Comparator<Measurement> {
+    @Override
+    public int compare(Measurement o1, Measurement o2) {
+        return o1.getDateTime().compareTo(o2.getDateTime());
+    }
+}
