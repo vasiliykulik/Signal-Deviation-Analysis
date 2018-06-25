74b330235fc5546dbf2b38a73f29044a71619a9d
Vasiliy Kylik <detroit.vv@gmail.com>
2018-06-25 20:02:50 +0300

    Comparators findMinUSSNR(Modem modem) and findMaxUSSNR(Modem modem)

--- main/java/ua/sda/comparators/ComparatorFindMaxSNR.java
+++ main/java/ua/sda/comparators/ComparatorFindMaxSNR.java
@@ -1,7 +1,19 @@
 package ua.sda.comparators;
 
+import ua.sda.entity.opticalnodeinterface.Measurement;
+
+import java.util.Comparator;
+
 /**
  * Created by Vasiliy Kylik (Lightning) on 25.06.2018.
  */
-public class ComparatorFindMaxSNR {
+public class ComparatorFindMaxSNR implements Comparator<Measurement> {
+    @Override
+    public int compare(Measurement o1, Measurement o2) {
+        int result = o1.getUsSNR().compareTo(o2.getUsSNR());
+        if (result!=0){
+            return result;
+        }
+        return 0;
+    }
 }
--- main/java/ua/sda/comparators/ComparatorFindMinSNR.java
+++ main/java/ua/sda/comparators/ComparatorFindMinSNR.java
@@ -1,7 +1,15 @@
 package ua.sda.comparators;
 
+import ua.sda.entity.opticalnodeinterface.Measurement;
+
+import java.util.Comparator;
+
 /**
  * Created by Vasiliy Kylik (Lightning) on 25.06.2018.
  */
-public class ComparatorFindMinSNR {
+public class ComparatorFindMinSNR implements Comparator<Measurement> {
+    @Override
+    public int compare(Measurement o1, Measurement o2) {
+        return 0;
+    }
 }
