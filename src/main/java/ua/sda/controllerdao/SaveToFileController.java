c4917dcbb264e42473f0243c6a63043e8972bef9
Raketa <detroi.vv@gmail.com>
2018-07-03 01:17:35 +0300

    writeToFile

---
+++ src/main/java/ua/sda/controllerdao/SaveToFileController.java
@@ -0,0 +1,29 @@
+package ua.sda.controllerdao;
+
+import ua.sda.entity.opticalnodeinterface.ModemDifferenceMeasurement;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vasiliy Kylik on(Rocket) on 03.07.2018.
+ */
+public class SaveToFileController {
+  public void writeToTxt(List<ModemDifferenceMeasurement> modems) {
+    List<String> lines = new ArrayList<>();
+    Path file = Paths.get("result " + modems.get(0).getStreet() + " " + modems.get(0).getHouseNumber());
+    for (ModemDifferenceMeasurement modem:modems){
+      lines.add(modem.toString());
+    }
+    try {
+      Files.write(file,lines, Charset.forName("UTF-8"));
+    } catch (IOException e) {
+      System.out.println("Failed to save File" + e);
+    }
+  }
+}
