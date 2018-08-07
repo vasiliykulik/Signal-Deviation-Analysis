fc396d8b2dc7d049914e40c91b5e4bcafe0a3d86
Raketa <detroi.vv@gmail.com>
2018-08-08 01:23:59 +0300

    MultiThreadRetrieveDataController.getCurrentStates()

--- src/main/java/ua/sda/controller/multithread/MTModemLocationReader.java
+++ src/main/java/ua/sda/controller/multithread/MTModemLocationReader.java
@@ -1,7 +1,19 @@
 package ua.sda.controller.multithread;
 
+import ua.sda.entity.multithreadentities.MultiThreadedLocation;
+
+import java.util.concurrent.Callable;
+
 /**
+ * The {@code MTModemLocationReader} class represents a dao
+ * to obtain Location for one Specific modem
+ *
  * @author Vasiliy Kylik on(Rocket) on 31.07.2018.
  */
-public class MTModemLocationReader {
+public class MTModemLocationReader implements Callable<MultiThreadedLocation> {
+
+    @Override
+    public MultiThreadedLocation call() throws Exception {
+        return null;
+    }
 }
